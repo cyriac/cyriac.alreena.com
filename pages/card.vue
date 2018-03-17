6664edfcd71ddced421db574d2636465a1538a28
Cyriac Thomas <me@cyriacthomas.com>
2018-03-17 21:52:22 +0530

    layout fixed

--- pages/card.vue
+++ pages/card.vue
@@ -1,37 +1,39 @@
 <template>
-  <div class="row justify-content-md-center">
-    <div class="col-lg-6 col-md-11 col-12">
-      <div class="card">
-        <div class="card-body">
-          <div class="row">
-            <div class="col-sm-9 col-12 text-right">
-              <img src="/img/leaf.svg" width="20%"/>
-              <div>with their families</div>
-            </div>
-          </div>
-          <div class="names">
+  <div class="container">
+    <div class="row justify-content-md-center">
+      <div class="col-lg-8 col-md-10 col-12">
+        <div class="card">
+          <div class="card-body">
             <div class="row">
               <div class="col-sm-9 col-12 text-right">
-                <div class="name">Alreena <span class="d-none d-sm-inline">Pinto</span></div>
-                <div class="d-block d-sm-none"><span class="amp">&</span></div>
-                <div class="name">Cyriac <span class="d-none d-sm-inline">Thomas</span></div>
-              </div>
-              <div class="col-sm-3 text-left d-none d-sm-block">
-                <span class="amp">&</span>
+                <img src="/img/leaf.svg" width="20%"/>
+                <div>with their families</div>
               </div>
             </div>
-          </div>
-          <div class="row">
-            <div class="col-sm-9 col-12 text-right">
-              will exchange their vows
+            <div class="names">
+              <div class="row">
+                <div class="col-sm-9 col-12 text-right">
+                  <div class="name">Alreena <span class="d-none d-sm-inline">Pinto</span></div>
+                  <div class="d-block d-sm-none"><span class="amp">&</span></div>
+                  <div class="name">Cyriac <span class="d-none d-sm-inline">Thomas</span></div>
+                </div>
+                <div class="col-sm-3 text-left d-none d-sm-block">
+                  <span class="amp">&</span>
+                </div>
+              </div>
             </div>
-          </div>
-          <div class="row">
-            <div class="col-sm-9 col-12 text-right">
-              <div class="dates">JANUARY 19 2019 • 17:30 PM</div>
-              <div>don bosco crypt • mumbai</div>
-              <div>reception to follow</div>
-              <div>taj lands end • mumbai</div>
+            <div class="row">
+              <div class="col-sm-9 col-12 text-right">
+                will exchange their vows
+              </div>
+            </div>
+            <div class="row">
+              <div class="col-sm-9 col-12 text-right">
+                <div class="dates">JANUARY 19 2019 • 17:30 PM</div>
+                <div>don bosco crypt • mumbai</div>
+                <div>reception to follow</div>
+                <div>taj lands end • mumbai</div>
+              </div>
             </div>
           </div>
         </div>
