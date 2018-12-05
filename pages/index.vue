7f62b3df5741f1e8c191112123b16d992db7bec5
Cyriac Thomas <cyriac@compile.com>
2018-12-05 23:16:55 +0530

    more realistic card decks

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,9 @@
 <template>
   <section class="container">
     <div class="card-group">
-      <Card src="/img/cover.jpg" class="inv-card-cover" :class="{open: activeCard == 'cover'}" @click.native="cardClick('cover')"/>
-      <Card src="/img/mumbai.jpg" class="inv-card-mumbai" :class="{open: activeCard == 'mumbai'}" @click.native="cardClick('mumbai')"/>
-      <Card src="/img/kochi.jpg" class="inv-card-kochi" :class="{open: activeCard == 'kochi'}" @click.native="cardClick('kochi')"/>
+      <Card src="/img/cover.jpg" class="inv-card inv-card-cover" :class="{open: activeCard == 'cover'}" @click.native="cardClick('cover')"/>
+      <Card src="/img/mumbai.jpg" class="inv-card inv-card-mumbai" :class="{open: activeCard == 'mumbai'}" @click.native="cardClick('mumbai')"/>
+      <Card src="/img/kochi.jpg" class="inv-card inv-card-kochi" :class="{open: activeCard == 'kochi'}" @click.native="cardClick('kochi')"/>
     </div>
   </section>
 </template>
@@ -42,32 +42,47 @@ export default {
   display: flex;
   max-width: 100%;
 }
-.inv-card-cover, .inv-card-mumbai, .inv-card-kochi {
+.inv-card {
   max-height: 40vh;
   position: fixed;
   bottom: 10vh;
   left: 10vw;
   transform-origin: bottom center;
   transition-duration: 1s;
-  border-radius: 5px !important;
+  border-radius: 2px !important;
   overflow: hidden !important;
   cursor: pointer;
   &.open {
     left: 40vw;
     max-height: 80vh;
     // z-index: 1001;
-    transform: rotate(0);
+    transform: rotate(0) !important;
   }
 }
-.inv-card-cover {
-  z-index: 1000;
-  transform: rotate(-10deg);
-}
-.inv-card-mumbai {
-  z-index: 999;
-}
-.inv-card-kochi {
-  z-index: 998;
-  transform: rotate(10deg);
+.card-group {
+  .inv-card-cover {
+    z-index: 1000;
+    transform: rotate(-5deg);
+  }
+  .inv-card-mumbai {
+    z-index: 999;
+  }
+  .inv-card-kochi {
+    z-index: 998;
+    transform: rotate(5deg);
+  }
+  &:hover {
+    .inv-card {
+      &:hover {
+        bottom: 12.5vh;
+      }
+    }
+    .inv-card-cover {
+      transform: rotate(-10deg);
+    }
+    .inv-card-kochi {
+      transform: rotate(10deg);
+    }
+  }
 }
 </style>
