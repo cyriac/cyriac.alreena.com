1c8241a15a8026851373f0a956c4db4900b6f9d8
Cyriac Thomas <cyriac@compile.com>
2018-12-07 00:25:59 +0530

    fixed title

--- nuxt.config.js
+++ nuxt.config.js
@@ -3,7 +3,7 @@ module.exports = {
   ** Headers of the page
   */
   head: {
-    title: 'cyriac.alreena.com',
+    title: 'Alreena and Cyriac',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
