f8c4dc6296b51823d8e7212a35385cc6510260e0
Cyriac Thomas <cyriac@compile.com>
2018-12-07 00:26:28 +0530

    fixed description

--- nuxt.config.js
+++ nuxt.config.js
@@ -7,7 +7,7 @@ module.exports = {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: 'cyriac.alreena.com' }
+      { hid: 'description', name: 'description', content: 'Alreena and Cyriac' }
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
